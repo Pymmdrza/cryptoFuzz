98c4623060ff9fc282faaf1305a55c3c3146f022
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2023-09-11 01:59:45 +0300

    Add files via upload

---
+++ cryptofuzz/b58.py
@@ -0,0 +1,136 @@
+from functools import lru_cache
+from hashlib import sha256
+from typing import Mapping, Union
+from assest import (
+    BITCOIN_ALPHABET as ALPHABET,
+    RIPPLE_ALPHABET as XRP_ALPHABET
+)
+
+
+def scrub_input(v: Union[str, bytes]) -> bytes:
+    if isinstance(v, str):
+        v = v.encode('ascii')
+
+    return v
+
+
+def b58encode_int(
+    i: int, default_one: bool = True, alphabet: bytes = ALPHABET
+) -> bytes:
+    """
+    Encode an integer using Base58
+    """
+    if not i and default_one:
+        return alphabet[0:1]
+    string = b""
+    base = len(alphabet)
+    while i:
+        i, idx = divmod(i, base)
+        string = alphabet[idx:idx+1] + string
+    return string
+
+
+def b58encode(
+    v: Union[str, bytes], alphabet: bytes = ALPHABET
+) -> bytes:
+    """
+    Encode a string using Base58
+    """
+    v = scrub_input(v)
+
+    mainSize = len(v)
+    v = v.lstrip(b'\0')
+    newSize = len(v)
+
+    acc = int.from_bytes(v, byteorder='big')  # first byte is most significant
+
+    result = b58encode_int(acc, default_one=False, alphabet=alphabet)
+    return alphabet[0:1] * (mainSize - newSize) + result
+
+
+@lru_cache()
+def _get_base58_decode_map(alphabet: bytes,
+                           autofix: bool) -> Mapping[int, int]:
+    invmap = {char: index for index, char in enumerate(alphabet)}
+
+    if autofix:
+        groups = [b'0Oo', b'Il1']
+        for group in groups:
+            pivots = [c for c in group if c in invmap]
+            if len(pivots) == 1:
+                for alternative in group:
+                    invmap[alternative] = invmap[pivots[0]]
+
+    return invmap
+
+
+def b58decode_int(
+    v: Union[str, bytes], alphabet: bytes = ALPHABET, *,
+    autofix: bool = False
+) -> int:
+    """
+    Decode a Base58 encoded string as an integer
+    """
+    if b' ' not in alphabet:
+        v = v.rstrip()
+    v = scrub_input(v)
+
+    map = _get_base58_decode_map(alphabet, autofix=autofix)
+
+    decimal = 0
+    base = len(alphabet)
+    try:
+        for char in v:
+            decimal = decimal * base + map[char]
+    except KeyError as e:
+        raise ValueError(
+            "Invalid character {!r}".format(chr(e.args[0]))
+        ) from None
+    return decimal
+
+
+def b58decode(
+    v: Union[str, bytes], alphabet: bytes = ALPHABET, *,
+    autofix: bool = False
+) -> bytes:
+    """
+    Decode a Base58 encoded string
+    """
+    v = v.rstrip()
+    v = scrub_input(v)
+
+    mainSize = len(v)
+    v = v.lstrip(alphabet[0:1])
+    newSize = len(v)
+
+    acc = b58decode_int(v, alphabet=alphabet, autofix=autofix)
+
+    return acc.to_bytes(mainSize - newSize + (acc.bit_length() + 7) // 8, 'big')
+
+
+def b58encode_check(
+    v: Union[str, bytes], alphabet: bytes = ALPHABET
+) -> bytes:
+    """
+    Encode a string using Base58 with a 4 character checksum
+    """
+    v = scrub_input(v)
+
+    digest = sha256(sha256(v).digest()).digest()
+    return b58encode(v + digest[:4], alphabet=alphabet)
+
+
+def b58decode_check(
+    v: Union[str, bytes], alphabet: bytes = ALPHABET, *,
+    autofix: bool = False
+) -> bytes:
+    """Decode and verify the checksum of a Base58 encoded string"""
+
+    result = b58decode(v, alphabet=alphabet, autofix=autofix)
+    result, check = result[:-4], result[-4:]
+    digest = sha256(sha256(result).digest()).digest()
+
+    if check != digest[:4]:
+        raise ValueError("Invalid checksum")
+
+    return result
---
+++ cryptofuzz/utils.py
@@ -0,0 +1,87 @@
+import binascii
+import os, re
+import hashlib, pbkdf2, hmac
+import hmac
+import random
+from hashlib import sha256 as SHA256, sha512 as SHA512, new as NEW
+import ecdsa
+from b58 import b58encode, b58encode_check, b58decode_check, b58decode
+from bit import Key
+from mnemonic import Mnemonic
+from bit.format import bytes_to_wif
+from assest import (
+    MAIN_DIGEST_RMD160,
+    MAIN_PREFIX,
+    MAIN_SUFFIX,
+    BIP39
+)
+
+
+def generate_entropy(entropy_bits=256):
+    entropy = os.urandom(entropy_bits // 8)
+    checksum = SHA256(entropy).digest()[0]
+    entropy_with_checksum = entropy + bytes([checksum])
+    return entropy_with_checksum
+
+
+def mne_to_seed(mnemonic, password=""):
+    salt = ("mnemonic" + password).encode('utf-8')
+    seed = hashlib.pbkdf2_hmac('sha512', mnemonic.encode('utf-8'), salt, 2048)
+    return seed[:64]
+
+
+def generate_keypair(seed):
+    sk = ecdsa.SigningKey.from_string(seed[:32], curve=ecdsa.SECP256k1)
+    vk = sk.get_verifying_key()
+    return sk, vk
+
+
+def pub_to_addr(pubkey, compressed=True):
+    if compressed:
+        if pubkey.to_string()[63] % 2 == 0:
+            compressed_pub = b"\x02" + pubkey.to_string()[:32]
+        else:
+            compressed_pub = b"\x03" + pubkey.to_string()[:32]
+    else:
+        compressed_pub = b"\x04" + pubkey.to_string()
+
+    ripemd160 = NEW('ripemd160')
+    ripemd160.update(SHA256(compressed_pub).digest())
+    raw_address = MAIN_DIGEST_RMD160 + ripemd160.digest()
+    address = b58encode(raw_address + SHA256(SHA256(raw_address).digest()).digest()[:4])
+    return address
+
+def byte2Mne(byte: bytes): return Mnemonic("english").to_mnemonic(byte[:32])
+
+
+def bytes2Wif(private_key, compress=True):
+    PREFIX = MAIN_PREFIX
+    if compress:
+        EXTENDED_KEY = PREFIX + private_key + MAIN_SUFFIX
+    else:
+        EXTENDED_KEY = PREFIX + private_key
+
+    FIRST_SHA256 = SHA256(EXTENDED_KEY).digest()
+    DOUBLE_SHA256 = SHA256(FIRST_SHA256).digest()
+    CHECKSUM = DOUBLE_SHA256[:4]
+
+    WIF = b58encode(EXTENDED_KEY + CHECKSUM)
+
+    return WIF.decode('utf-8')
+
+
+def generate_mnemonic(size: int) -> str:
+    characters = re.findall('[A-Z][a-z]+', BIP39)
+    return " ".join(random.choices(characters, k=size)).lower()
+
+
+word = generate_mnemonic(12)
+
+seed = mne_to_seed(word)
+
+private, public = generate_keypair(seed)
+
+caddr = pub_to_addr(public, True)
+uaddr = pub_to_addr(public, False)
+print(word)
+print(f"compress: {caddr}\nUncompress: {uaddr}\n{private.to_string()}")
