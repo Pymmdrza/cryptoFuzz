6dad0e7157b47c56cdb01851fe2c175724f7fd0b
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2023-09-26 04:43:55 +0300

    Update install.py

--- install.py
+++ install.py
@@ -2,13 +2,6 @@ import os
 import sys
 import subprocess
 import platform
-from colorthon import Colors
-
-red = Colors.RED
-green = Colors.GREEN
-yellow = Colors.YELLOW
-reset = Colors.RESET
-
 
 def subInstall(package_name: str):
     if "win" in platform.platform().lower():
@@ -27,26 +20,7 @@ def install_deps():
         print(f" {green}Installing{reset}:{yellow} {dep}{reset}")
         subInstall(dep)
 
-def colorthon():
-    if "win" in platform.platform().lower():
-        print(f"{red}Installing{reset}:{yellow} Colorthon{reset}")
-        subprocess.run(['pip', 'install', 'colorthon'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(f"{green}Successfully installed{reset}: Colorthon")
-
-    elif "linux" in platform.platform().lower():
-        print(f"{red}Installing{reset}:{yellow} Colorthon{reset}")
-        subprocess.run(['pip3', 'install', 'colorthon'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(f"{green}Successfully installed{reset}: Colorthon")
-
-    elif "mac" in platform.platform().lower():
-        print(f"{red}Installing{reset}:{yellow} Colorthon{reset}")
-        subprocess.run(['pip3', 'install', 'colorthon'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(f"{green}Successfully installed{reset}: Colorthon")
-
-
-
 def main():
-    colorthon()
     install_deps()
     print(f"{red}CryptoFuzz{reset} {green}With dependencies have been installed!{reset}")
 
