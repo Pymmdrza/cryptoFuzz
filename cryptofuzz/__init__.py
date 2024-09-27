cb507f775dd9b64b07c8813a668cc5893ca4f0bc
MMDRZA <info@mmdrza.com>
2024-09-27 23:48:28 +0300

    Update __init__.py

--- cryptofuzz/__init__.py
+++ cryptofuzz/__init__.py
@@ -14,6 +14,7 @@ from .hd import (
     Qtum,
     Ravencoin,
     Tron,
+    Ton,
     Zcash,
 )
 
@@ -59,5 +60,6 @@ __all__ = [
     "Qtum",
     "Ravencoin",
     "Tron",
+    "Ton",
     "Zcash"
 ]
