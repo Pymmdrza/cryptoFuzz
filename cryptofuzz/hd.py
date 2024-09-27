5f07011338d984278cd42d28baaed284d291a6f0
MMDRZA <info@mmdrza.com>
2024-09-27 23:45:21 +0300

    :white_check_mark: Add TON Method
    :white_check_mark: Added TON Address Generator & Convertor

--- cryptofuzz/hd.py
+++ cryptofuzz/hd.py
@@ -1,4 +1,10 @@
 from Crypto.Hash import keccak
+from hmac import new as _new
+from hashlib import (
+    pbkdf2_hmac as _pbkdf2_hmac,
+    sha256 as _sha256
+)
+from base64 import urlsafe_b64encode as _urlsafe_b64encode
 from .bs58 import b58encode_check
 from .utils import (
     re,
@@ -19,6 +25,41 @@ from .utils import (
     hashlib,
     MAX_PRIVATE_KEY
 )
+
+CRC16_TAB = [
+    0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50A5, 0x60C6, 0x70E7,
+    0x8108, 0x9129, 0xA14A, 0xB16B, 0xC18C, 0xD1AD, 0xE1CE, 0xF1EF,
+    0x1231, 0x0210, 0x3273, 0x2252, 0x52B5, 0x4294, 0x72F7, 0x62D6,
+    0x9339, 0x8318, 0xB37B, 0xA35A, 0xD3BD, 0xC39C, 0xF3FF, 0xE3DE,
+    0x2462, 0x3443, 0x0420, 0x1401, 0x64E6, 0x74C7, 0x44A4, 0x5485,
+    0xA56A, 0xB54B, 0x8528, 0x9509, 0xE5EE, 0xF5CF, 0xC5AC, 0xD58D,
+    0x3653, 0x2672, 0x1611, 0x0630, 0x76D7, 0x66F6, 0x5695, 0x46B4,
+    0xB75B, 0xA77A, 0x9719, 0x8738, 0xF7DF, 0xE7FE, 0xD79D, 0xC7BC,
+    0x48C4, 0x58E5, 0x6886, 0x78A7, 0x0840, 0x1861, 0x2802, 0x3823,
+    0xC9CC, 0xD9ED, 0xE98E, 0xF9AF, 0x8948, 0x9969, 0xA90A, 0xB92B,
+    0x5AF5, 0x4AD4, 0x7AB7, 0x6A96, 0x1A71, 0x0A50, 0x3A33, 0x2A12,
+    0xDBFD, 0xCBDC, 0xFBBF, 0xEB9E, 0x9B79, 0x8B58, 0xBB3B, 0xAB1A,
+    0x6CA6, 0x7C87, 0x4CE4, 0x5CC5, 0x2C22, 0x3C03, 0x0C60, 0x1C41,
+    0xEDAE, 0xFD8F, 0xCDEC, 0xDDCD, 0xAD2A, 0xBD0B, 0x8D68, 0x9D49,
+    0x7E97, 0x6EB6, 0x5ED5, 0x4EF4, 0x3E13, 0x2E32, 0x1E51, 0x0E70,
+    0xFF9F, 0xEFBE, 0xDFDD, 0xCFFC, 0xBF1B, 0xAF3A, 0x9F59, 0x8F78,
+    0x9188, 0x81A9, 0xB1CA, 0xA1EB, 0xD10C, 0xC12D, 0xF14E, 0xE16F,
+    0x1080, 0x00A1, 0x30C2, 0x20E3, 0x5004, 0x4025, 0x7046, 0x6067,
+    0x83B9, 0x9398, 0xA3FB, 0xB3DA, 0xC33D, 0xD31C, 0xE37F, 0xF35E,
+    0x02B1, 0x1290, 0x22F3, 0x32D2, 0x4235, 0x5214, 0x6277, 0x7256,
+    0xB5EA, 0xA5CB, 0x95A8, 0x8589, 0xF56E, 0xE54F, 0xD52C, 0xC50D,
+    0x34E2, 0x24C3, 0x14A0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
+    0xA7DB, 0xB7FA, 0x8799, 0x97B8, 0xE75F, 0xF77E, 0xC71D, 0xD73C,
+    0x26D3, 0x36F2, 0x0691, 0x16B0, 0x6657, 0x7676, 0x4615, 0x5634,
+    0xD94C, 0xC96D, 0xF90E, 0xE92F, 0x99C8, 0x89E9, 0xB98A, 0xA9AB,
+    0x5844, 0x4865, 0x7806, 0x6827, 0x18C0, 0x08E1, 0x3882, 0x28A3,
+    0xCB7D, 0xDB5C, 0xEB3F, 0xFB1E, 0x8BF9, 0x9BD8, 0xABBB, 0xBB9A,
+    0x4A75, 0x5A54, 0x6A37, 0x7A16, 0x0AF1, 0x1AD0, 0x2AB3, 0x3A92,
+    0xFD2E, 0xED0F, 0xDD6C, 0xCD4D, 0xBDAA, 0xAD8B, 0x9DE8, 0x8DC9,
+    0x7C26, 0x6C07, 0x5C64, 0x4C45, 0x3CA2, 0x2C83, 0x1CE0, 0x0CC1,
+    0xEF1F, 0xFF3E, 0xCF5D, 0xDF7C, 0xAF9B, 0xBFBA, 0x8FD9, 0x9FF8,
+    0x6E17, 0x7E36, 0x4E55, 0x5E74, 0x2E93, 0x3EB2, 0x0ED1, 0x1EF0
+]
 
 
 def is_valid_hex(hexstring: str) -> bool:
@@ -142,15 +183,6 @@ class DigiByte:
         :rtype str:
         :return: Str - address
 
-
-        --------------------------------------------------------------
-
-        >>> dgb = DigiByte()
-        >>> privatekey = "0A97965...A45517" # example Private Key
-        >>> digibyte_address = dgb.hex_addr(privatekey)
-
-        --------------------------------------------------------------
-
         """
         if is_valid_hex(hexed):
             hd: HD_W = HD_W(DGB)
@@ -174,17 +206,7 @@ class Dogecoin:
         :type Type: str
         :rtype: str
         :return: str - address
-
-
-        --------------------------------------------------------------
-
-        >>> doge = Dogecoin()
-        >>> privatekey = "0A9796542F1030...02F6A45517" # example Private Key
-        >>> p2pkh_doge_addr = doge.hex_addr(privatekey, 'p2pkh')
-        >>> p2sh_doge_addr = doge.hex_addr(privatekey, 'p2sh')
-
-        --------------------------------------------------------------
-
+        
         """
 
         if is_valid_hex(hexed):
@@ -410,3 +432,129 @@ class Axe:
             return hd.p2pkh_address()
         else:
             ValueError("hex format invalid check again.[format: hex][64 length]")
+
+
+class Ton:
+    """
+    TON Address Generator for Mnemonic and Private Key inputs.
+    Supports both Raw and User-Friendly Address formats.
+
+    # https://docs.ton.org/learn/overviews/addresses#address-of-smart-contract
+    # https://docs.ton.org/learn/overviews/addresses#user-friendly-address-encoding-examples
+
+    """
+
+    def __init__(self, workchain: int = 0, mainnet: bool = True):
+        """
+        Initialize the TON Address Generator.
+
+        :param workchain: Workchain ID for address (0 for basic workchain, -1 for masterchain).
+        :param mainnet: Specify if the address is for the mainnet (True) or testnet (False).
+        https://docs.ton.org/learn/overviews/addresses#user-friendly-address-structure
+
+        """
+        self.WORKCHAIN = workchain
+        self.mainnet = mainnet
+
+    @staticmethod
+    def _mnemonic_to_seed(in_mnemonic: str, in_passphrase: str = "") -> bytes:
+        """
+        Convert mnemonic to a 64-byte seed using the BIP-39 standard.
+
+        :param in_mnemonic: Mnemonic string (24 words).
+        :param in_passphrase: Optional passphrase for mnemonic.
+        :return: Seed derived from the mnemonic.
+        """
+        salt = "mnemonic" + in_passphrase
+        return _pbkdf2_hmac("sha512", in_mnemonic.encode(), salt.encode(), 2048)
+
+    def _mnemonic_to_publickey(self, in_Target: str) -> bytes:
+        """
+        Convert mnemonic to public key bytes.
+
+        :param in_Target: Mnemonic string.
+        :return: Public key as bytes.
+        """
+        seed = self._mnemonic_to_seed(in_mnemonic=in_Target)
+        _key = _new(b"TON seed", seed, hashlib.sha512).digest()[:32]
+        return _sha256(_key).digest()
+
+    @staticmethod
+    def _pvk_to_publickey(in_Target: str) -> bytes:
+        """
+        Convert private key to public key bytes.
+
+        :param in_Target: Private key in hexadecimal format.
+        :return: Public key as bytes.
+        :raises ValueError: If the private key is invalid.
+        """
+        if len(in_Target) == 64 and all(c in '0123456789abcdefABCDEF' for c in in_Target):
+            _key = bytes.fromhex(in_Target)
+            return hashlib.sha256(_key).digest()
+        raise ValueError("Invalid private key format. Must be a 64-character hex string.")
+
+    @staticmethod
+    def cal_crc16(data: bytes) -> bytes:
+        """Calculate CRC16-CCITT checksum."""
+        crc = 0xFFFF
+        for byte in data:
+            tbl_idx = ((crc >> 8) ^ byte) & 0xff
+            crc = ((crc << 8) ^ CRC16_TAB[tbl_idx]) & 0xffff
+        return crc.to_bytes(2, 'big')
+
+    def publickey_to_address(self, publickey: bytes, bounceable: bool = True) -> str:
+        """
+        Convert public key to a user-friendly TON address.
+        # https://docs.ton.org/learn/overviews/addresses#bounceable-vs-non-bounceable-addresses
+
+        :param publickey: Public key bytes.
+        :param bounceable: If True, generates a bounceable address.
+        :return: User-friendly TON address in Base64 format.
+
+        """
+        tag_byte = 0x11 if bounceable else 0x51
+        if not self.mainnet:
+            tag_byte += 0x80
+
+        # Prepare the address bytes
+        workchain_byte = 0xff if self.WORKCHAIN == -1 else 0x00
+        addr_data = bytes([tag_byte]) + bytes([workchain_byte])
+        addr_data += publickey
+        raw_addr = addr_data + self.cal_crc16(addr_data)
+        # Encode the address in Base64 URL-Safe
+        return _urlsafe_b64encode(raw_addr).decode('utf-8').rstrip('=')
+
+    def mnemonic_to_address(self, in_mnemonic: str, bounceable: bool = True) -> str:
+        """
+        Convert mnemonic to a TON wallet address.
+
+        :param in_mnemonic: Mnemonic string (24 words).
+        :param bounceable: If True, generates a bounceable address.
+        :return: User-friendly TON address in Base64 format.
+        :raises ValueError: If the mnemonic is invalid.
+        """
+        if not isinstance(in_mnemonic, str) or len(in_mnemonic.split()) != 24:
+            raise ValueError("Invalid mnemonic. Must be 24 words.")
+        pub = self._mnemonic_to_publickey(in_mnemonic)
+        return self.publickey_to_address(pub, bounceable)
+
+    def privatekey_to_address(self, in_privatekey: str, bounceable: bool = True) -> str:
+        """
+        Convert private key to a TON wallet address.
+
+        :param in_privatekey: Private key in hexadecimal format.
+        :param bounceable: If True, generates a bounceable address.
+        :return: User-friendly TON address in Base64 format.
+        :raises ValueError: If the private key is invalid.
+        """
+        pub = self._pvk_to_publickey(in_privatekey)
+        return self.publickey_to_address(pub, bounceable)
+
+    def decimal_to_address(self, dec: int, bounceable: bool = True) -> str:
+        """
+        Convert decimal integer to a TON wallet address.
+        """
+        if dec > MAX_PRIVATE_KEY:
+            raise ValueError(f"\nInvalid Decimal Value for Private Key, Must be Less Than {MAX_PRIVATE_KEY}\n")
+        pvk = "%064x" % dec
+        return self.privatekey_to_address(pvk, bounceable)
